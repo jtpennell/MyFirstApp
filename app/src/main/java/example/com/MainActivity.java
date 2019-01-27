bd76d88ae4ba8f2baced2f6831b430f8d00207ee
JT <jayt24seven@gmail.com>
2019-01-28 10:34:27 +1300

    Commit Directory

---
+++ app/src/main/java/example/com/MainActivity.java
@@ -0,0 +1,30 @@
+package example.com;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "example.com.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // Called when the user taps the Send button
+    public void sendMessage(View view){
+        // do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+        
+    }
+
+}
